7ab035ccd9d6a8ed01b49e834728bc13432c9322
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-11-01 13:11:24 +0000

    replacing the title of tvshows with their cover art

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import filmCover from './images/Jumanji.png'
+import tvshow1 from './images/breakingbad.jpg';
+import tvshow2 from './images/deathnote.jpg';
+import tvshow3 from './images/blackmirror.jpg';
+import tvshow4 from './images/got.jpg';
+import tvshow5 from './images/thewire.jpg';
+import tvshow6 from './images/thewalkingdead.png';
+
 import './App.css';
 
 class App extends Component {
@@ -9,15 +15,15 @@ class App extends Component {
     return (
       <div className="App">
         <div class="flex-grid-thirds">
-          <div class="col">Black Mirror</div>
-          <div class="col">Breaking Bad</div>
-          <div class="col">Death Note</div>
+          <div class="col"><img src={tvshow1} className="film-cover" alt="filmCover" /></div>
+          <div class="col"><img src={tvshow2} className="film-cover" alt="filmCover" /></div>
+          <div class="col"><img src={tvshow3} className="film-cover" alt="filmCover" /></div>
         </div>
 
         <div class="flex-grid-thirds">
-          <div class="col">Game of Thrones</div>
-          <div class="col">The Walking Dead</div>
-          <div class="col">The wire</div>
+          <div class="col"><img src={tvshow4} className="film-cover" alt="filmCover" /></div>
+          <div class="col"><img src={tvshow5} className="film-cover" alt="filmCover" /></div>
+          <div class="col"><img src={tvshow6} className="film-cover" alt="filmCover" /></div>
         </div>
       </div>
     );
