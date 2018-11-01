6931eaf65c1d256f7127da07be91ee317d9bb57f
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-11-01 12:12:37 +0000

    displaying tv show titles in 2 rows by 3 columns

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import logo from './logo.svg';
 import filmCover from './images/Jumanji.png'
 import './App.css';
 
-console.log(logo);
 class App extends Component {
   
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to dokflix</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <img src={filmCover} className="film-cover" alt="filmCover" />
+        <div class="flex-grid-thirds">
+          <div class="col">Black Mirror</div>
+          <div class="col">Breaking Bad</div>
+          <div class="col">Death Note</div>
+        </div>
+
+        <div class="flex-grid-thirds">
+          <div class="col">Game of Thrones</div>
+          <div class="col">The Walking Dead</div>
+          <div class="col">The wire</div>
+        </div>
       </div>
     );
   }
