045fc9d1bbf827e1946eda9cdca9f272e9a3069e
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-11-01 10:57:56 +0000

    adding image to my kodflix page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import filmCover from './images/Jumanji.png'
 import './App.css';
 
+console.log(logo);
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
@@ -13,6 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src={filmCover} className="film-cover" alt="filmCover" />
       </div>
     );
   }
