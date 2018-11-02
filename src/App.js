b4ebc0b107799dfa94867bf82fee940105ef1f2f
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-11-02 10:34:37 +0000

    adding title-overlay which appear on hover to the film-covers

--- src/App.js
+++ src/App.js
@@ -15,15 +15,45 @@ class App extends Component {
     return (
       <div className="App">
         <div class="flex-grid-thirds flex-grid">
-          <div class="col"><img src={tvshow1} className="film-cover" alt="filmCover" /></div>
-          <div class="col"><img src={tvshow2} className="film-cover" alt="filmCover" /></div>
-          <div class="col"><img src={tvshow3} className="film-cover" alt="filmCover" /></div>
+          <div class="overlay">
+            <div class="col"><img src={tvshow1} className="film-cover fade-image" alt="filmCover" /></div>
+            <div class="title-panel">
+              <div class="title">Breaking Bad</div>
+            </div>
+          </div>
+          <div class="overlay">
+            <div class="col"><img src={tvshow2} className="film-cover fade-image" alt="filmCover" /></div>
+            <div class="title-panel">
+              <div class="title">Death Note</div>
+            </div>
+          </div>
+          <div class="overlay">
+            <div class="col"><img src={tvshow3} className="film-cover fade-image" alt="filmCover" /></div>
+            <div class="title-panel">
+              <div class="title">Black Mirror</div>
+            </div>
+          </div>
         </div>
 
         <div class="flex-grid-thirds flex-grid">
-          <div class="col"><img src={tvshow4} className="film-cover" alt="filmCover" /></div>
-          <div class="col"><img src={tvshow5} className="film-cover" alt="filmCover" /></div>
-          <div class="col"><img src={tvshow6} className="film-cover" alt="filmCover" /></div>
+        <div class="overlay">
+            <div class="col"><img src={tvshow4} className="film-cover fade-image" alt="filmCover" /></div>
+            <div class="title-panel">
+              <div class="title">Game Of Thrones</div>
+            </div>
+          </div>
+          <div class="overlay">
+            <div class="col"><img src={tvshow5} className="film-cover fade-image" alt="filmCover" /></div>
+            <div class="title-panel">
+              <div class="title">The Wire</div>
+            </div>
+          </div>
+          <div class="overlay">
+            <div class="col"><img src={tvshow6} className="film-cover fade-image" alt="filmCover" /></div>
+            <div class="title-panel">
+              <div class="title">The Walking Dead</div>
+            </div>
+          </div>
         </div>
       </div>
     );
