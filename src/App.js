03eaff24bb06d16b35c8610187e643ab998246e5
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-11-06 13:21:36 +0000

    adding Gallery component which contain all the Cover components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React, { Component } from "react";
-import {Cover} from './Cover.js';
-import tvshow1 from "./images/hillhouse.jpg";
-import tvshow2 from "./images/deathnote.jpg";
-import tvshow3 from "./images/blackmirror.jpg";
-import tvshow4 from "./images/got.jpg";
-import tvshow5 from "./images/thewire.jpg";
-import tvshow6 from "./images/thewalkingdead.png";
+import {Gallery} from './Gallery.js';
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div class="flex-grid-thirds flex-grid">
-          <Cover name="The Haunting of Hill House" tvshow = {tvshow1} />
-          <Cover name="Death Note" tvshow = {tvshow2} />
-          <Cover name="Black Mirror" tvshow = {tvshow3}/>
-        </div>
-        <div class="flex-grid-thirds flex-grid">
-          <Cover name="Game Of Thrones" tvshow = {tvshow4}/>
-          <Cover name="The Wire" tvshow = {tvshow5} />
-          <Cover name="The Walking Dead" tvshow = {tvshow6}/>
-        </div>
-    </div>
+        <Gallery />
+      </div>
     );
   }
 }
---
+++ src/Gallery.js
@@ -0,0 +1,23 @@
+import React from "react";
+import tvshow1 from "./images/hillhouse.jpg";
+import tvshow2 from "./images/deathnote.jpg";
+import tvshow3 from "./images/blackmirror.jpg";
+import tvshow4 from "./images/got.jpg";
+import tvshow5 from "./images/thewire.jpg";
+import tvshow6 from "./images/thewalkingdead.png";
+import {Cover} from './Cover.js';
+
+function Gallery(props) {
+    return (
+        <div class="flex-grid-thirds flex-grid">
+            <Cover name="The Haunting of Hill House" tvshow = {tvshow1} />
+            <Cover name="Death Note" tvshow = {tvshow2} />
+            <Cover name="Black Mirror" tvshow = {tvshow3}/>
+            <Cover name="Game Of Thrones" tvshow = {tvshow4}/>
+            <Cover name="The Wire" tvshow = {tvshow5} />
+            <Cover name="The Walking Dead" tvshow = {tvshow6} />
+        </div>
+      );
+}
+
+export {Gallery};
