29a136926c3b07a068274aaf65a7b979d9a2c2e0
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-11-12 14:11:05 +0000

    changed positioning of closing parenthesis for return statement on a new line

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ class App extends Component {
               <Route path='/:details' component={Details} />
             </Switch>
           </BrowserRouter>
-      </div>);
+      </div>
+    );
   }
 }
 
