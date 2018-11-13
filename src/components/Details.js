f9361589c5f689accfb760eab0cecbde3540d493
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-11-13 15:25:32 +0000

    Show the TV show title on the details page by finding the showid

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,24 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import GetGallery from '../get-gallery';
 
 class Details extends React.Component {
   constructor() {
     super();
+    //Save the Id of the current TV show
     this.state = {
-      message: "Hello, this will be the details page for each Movie & TV show"
-    };
+        currentShowID: '',
+        currentShowName: ''
+    }
   }
-  
-  componentWillMount = () => {
-      setTimeout(() => { //use arrow function, opposed to classic function definition as timeout has issues with the latter syntax
-        this.setState({ message: 'coming soon '})
-      }, 3000)
+
+  componentDidMount = () => {
+    this.setState({
+        currentShowID: GetGallery().find((tvshow) => {
+            return tvshow.showid === this.props.match.params.details;
+        }).showid,
+        currentShowName: GetGallery().find((tvshow) => {
+            return tvshow.showid === this.props.match.params.details;
+        }).name
+    })
   };
 
   render() {
     return (
+        
       <div className="Details">
-        <p>{this.state.message}</p>
+        <h2>{this.state.currentShowName}</h2> 
         <Link to="/">Return back to Media Gallery.</Link>
       </div>
     );
