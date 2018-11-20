923db40a2537657f3e88df63b36e7992d6d28dae
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-11-20 18:24:45 +0000

    creating a function that returns list of shows to backend

---
+++ src/backend/shows.js
@@ -0,0 +1,10 @@
+module.exports = function() {
+    return [
+        { name: 'The Haunting of Hill House', showid:'Haunting-Hill-House', synopsis: 'this show is scary' },
+        { name: 'Death Note', showid:'Death-Note', synopsis: 'this show is thrilling' },
+        { name: 'Black Mirror', showid:'Black-Mirror', synopsis: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.' },
+        { name: 'Game Of Thrones', showid:'Game-Of-Thrones', synopsis: 'this show is unpredictable' },
+        { name: 'The Wire', showid:'The-Wire', synopsis: 'this show has gangsters' },
+        { name: 'The Walking Dead', showid:'The-Walking-Dead', synopsis: 'this show has zombies' },
+    ];
+};
