962c5f0e3e197ec7c13e4c316a7a3c849a2439f7
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-11-21 11:23:03 +0000

    changed app.js, backend port to 3001 to allow for multiple servers

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const shows = require('./shows') //imports the array of show objects
 
 app.get('/rest/shows', (req, res) => res.send(shows())) //the url, what to send pack
