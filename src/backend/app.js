d35f565a048a0810dfc94d5226be1f4db4108fe8
Yu-Jin <yujin_1991@hotmail.com>
2018-12-04 14:11:37 +0000

    Enable backend in prod (#26)
    * adding hot module replacement to index.js
    
    * fetching shows from the backend
    
    * deploying to heroku

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const path = require('path')
+const port = process.env.PORT || 3001;
 const shows = require('./shows') //imports the array of show objects
 
-app.get('/rest/shows', (req, res) => res.send(shows())) //the url, what to send pack
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/rest/shows', (req, res) => res.send(shows())) //the url, what to send back
+app.use(express.static(path.join(__dirname, '../../build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
+// app.get('/', function (req, res) {
+//   res.send('hello world')
+// })
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
