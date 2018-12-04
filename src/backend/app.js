5e80afbc2380206275c18cd93c178f27c7364a10
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-12-04 14:02:10 +0000

    deploying to heroku

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,14 +4,12 @@ const path = require('path')
 const port = process.env.PORT || 3001;
 const shows = require('./shows') //imports the array of show objects
 
-//the url, what to send back
-app.get('/rest/shows', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
-    res.send(shows());
-});
+app.get('/rest/shows', (req, res) => res.send(shows())) //the url, what to send back
 app.use(express.static(path.join(__dirname, '../../build')));
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
 // app.get('/', function (req, res) {
-//     res.send('hello world')
+//   res.send('hello world')
 // })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
