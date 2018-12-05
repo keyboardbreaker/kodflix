ac01d3a3cbfa485c7f18fe42df99462c00fd7bff
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-12-05 14:25:06 +0000

    removing importing images from frontend

--- src/frontend/components/Gallery.js
+++ src/frontend/components/Gallery.js
@@ -1,12 +1,6 @@
 import React from "react";
 import getGallery from '../get-gallery.js';
 import { Cover } from './Cover.js';
-import tvshow1 from "../images/hillhouse.jpg";
-// import tvshow2 from "./images/deathnote.jpg";
-// import tvshow3 from "./images/blackmirror.jpg";
-// import tvshow4 from "./images/got.jpg";
-// import tvshow5 from "./images/thewire.jpg";
-// import tvshow6 from "./images/thewalkingdead.png";
 
 class Gallery extends React.Component {
     constructor() {
