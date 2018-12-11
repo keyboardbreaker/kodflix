ca57150f2b744c8f937f47867c293ba5a0dc739b
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-12-11 13:57:29 +0000

    adding a centered loading indicator to details page

--- src/frontend/components/Details.js
+++ src/frontend/components/Details.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link, Redirect } from "react-router-dom";
 import GetGallery from "../get-gallery";
 import "./Details.css";
+import { Loading } from "./Loading";
+import "./Loading.css";
 
 class Details extends React.Component {
   constructor() {
@@ -9,7 +11,8 @@ class Details extends React.Component {
     //Save the Id of the current TV show
     this.state = {
       currentShow: {},
-      toNotFound: false
+      toNotFound: false,
+      isLoading: true
     };
   }
 
@@ -21,37 +24,39 @@ class Details extends React.Component {
           console.log(tvshow);
           return tvshow.showid === this.props.match.params.details;
         });
-        //this.setState({ currentShow: findShow });
         if (!findShow) {
           this.setState({
-            toNotFound: true
+            toNotFound: true,
+            isLoading: false
           });
         }
-        //checking for currentShow to be empty 
+        //checking for currentShow to be empty
         else if (Object.keys(this.state.currentShow).length === 0) {
           //show exists
           this.setState({
             toNotFound: false,
-            currentShow: findShow
+            currentShow: findShow,
+            isLoading: false
           });
         }
       });
   };
 
   render() {
-    if (this.state.toNotFound === true) {
-      return <Redirect to="/NotFound" />;
-    } else {
-      return (
-        <div className="Details">
-          <h2>{ this.state.currentShow.name }</h2>
-          <Link to="/">Return back to Media Gallery.</Link>
-          <div className="container">
-            <div className="column"><p>{this.state.currentShow.synopsis}</p></div>
-          </div>
+    return this.state.toNotFound ? (
+      <Redirect to="/NotFound" />
+    ) : this.state.isLoading ? (
+      <Loading />
+    ) : (
+      <div className="Details">
+        <h2>{this.state.currentShow.name}</h2>
+        <Link to="/">Return back to Media Gallery.</Link>
+        <div className="container">
+          <div className="column"><img src={require(`../common/images/${this.state.currentShow.showid}.jpg`)} alt="detailsCover"/></div>
+          <div className="column"><p>{this.state.currentShow.synopsis}</p></div>
         </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
