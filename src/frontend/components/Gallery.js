f664937c07e395251e41b060b1ec92fcaa1be89e
Yu-Jin <yujin_1991@hotmail.com>
2018-12-11 13:59:41 +0000

    Creating loading indicator (#31)
    * renaming the browser title to LatestFlix
    
    * adding loading indicator component
    
    * adding a centered loading indicator to details page
    
    * adding loading component to gallery page

--- src/frontend/components/Gallery.js
+++ src/frontend/components/Gallery.js
@@ -1,36 +1,44 @@
 import React from "react";
 import getGallery from '../get-gallery.js';
 import { Cover } from './Cover.js';
+import { Loading } from "./Loading";
+import "./Loading.css";
 
 class Gallery extends React.Component {
     constructor() {
         super();
         this.state = {
             shows: [],
+            isLoading: true
         }
     }
-    
+
     componentDidMount = () => {
         fetch(getGallery())
           .then(res => res.json())
           .then(shows => {
             //   console.log(shows);
-              this.setState({shows: shows});
+              this.setState({
+                  shows: shows,
+                  isLoading: false
+            });
           });
     }
 
     render() {
         return (
-            <div className="flex-grid">
-                {
-                    this.state.shows.map((show, index) => {
-                        return  <Cover key={ index } 
-                                    name={ show.name } 
-                                    tvshow={ show.tvshow } 
-                                    showid={ show.showid }/>;
-                    })
-                }
-            </div>
+            this.state.isLoading ? 
+                <Loading/> : 
+                <div className="flex-grid">
+                    {
+                        this.state.shows.map((show, index) => {
+                            return  <Cover key={ index } 
+                                        name={ show.name } 
+                                        tvshow={ show.tvshow } 
+                                        showid={ show.showid }/>;
+                        })
+                    }
+                </div>
         );
     }
 }
